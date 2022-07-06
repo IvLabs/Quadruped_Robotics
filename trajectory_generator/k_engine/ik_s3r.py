61280dcee5dc789b34804035194dd4871a34ca80
Nachiket497 <1000nachiket@gmail.com>
2022-07-07 04:00:42 +0530

    ik modified

--- trajectory_generator/k_engine/ik_s3r.py
+++ trajectory_generator/k_engine/ik_s3r.py
@@ -11,25 +11,15 @@ class in_kin():
 
         theta_1 = m.atan2(y,x)
 
-        x1 = x/m.cos(theta_1) - self.l1
-        z1 = z
-        
-        r_2 = x1**2 + z1**2
-        
-        a = r_2 - (self.l2**2 + self.l3**2)
-        # n=2*self.l2*self.l3
+        x1 = self.l1*m.cos(theta_1)
+        y1 = self.l1*m.sin(theta_1)
+        z1 = 0
 
-        c = a/(2*self.l2*self.l3)
+        r = m.sqrt((x-x1)**2 + (y-y1)**2 + (z-z1)**2)
 
-        theta_3 = m.acos(c)
+        theta_2 = m.acos((self.l2**2 + r**2 - self.l3**2)/(2*self.l2*r))
 
-        l3c3 = self.l3*m.cos(theta_3)
-        l3s3 = self.l3*m.sin(theta_3)
-        
-        beta = m.atan2(l3s3, self.l2 + l3c3)
-        gamma = m.atan2(z1, x1)
-        
-        theta_2 = gamma - beta
+        theta_3 = m.pi - m.acos((self.l3**2 +  self.l2**2 - r**2)/(2*self.l3* self.l2))
 
         return theta_1,theta_2,theta_3
 
