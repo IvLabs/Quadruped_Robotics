ba4f210df17642c14d76f560d5d4b0831acc9ab0
yaswanth1701 <gonnayaswanth17@gmail.com>
2022-07-10 02:38:18 +0530

    code for phase added

--- test/traj.py
+++ test/traj.py
@@ -27,6 +27,14 @@ class trajectory_generator():
         self.ang1_shift=args[0]
 
     def get_next( self):
+        n_max=int(self.f_hard/(2*self.f_gate))
+        if n_max<=self.n:
+            r=int(self.n/n_max)
+            self.n=self.n-r*n_max
+            self.flag=-1*self.flag
+            if( self.flag == -1 ):
+                self.update_vx_vy()
+            self.swap();
 
         time_left = ( (1/(2*self.f_gate))  - ( self.n/self.f_hard)   ) 
         if time_left > 0 :
